6e408f7059dd87a102971d1349e835530f007d2f
daniele-aceti <daniele.aceti@hotmail.it>
2025-01-08 16:40:37 +0100

    feat: ex 3

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -14,7 +14,20 @@ const animal = [
         { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
 ]
 
+//CREO UN ARRAY MAMMIFERI COSI DA POTERLA RIUTILIZZARE FUORI DAL BLOCCO FOR
 const mammiferi = []
 
-console.log(animal.classe)
+//FACCIO UN FOR IN PER CICLARE L'ARRAY COSI DA POTER FILTRARE I MAMMIFERI 
 
+for (const key in animal) {
+        const element = animal[key];
+        if(element.classe === "mammiferi"){
+            mammiferi.push(element)
+            //console.log(mammiferi)
+        }else{
+            console.log("non è un mammifero")
+        }
+}
+
+//ARRAY MAMMIFERI ORA CON LA LISTA DEI MAMMIFERI
+console.log(mammiferi)
